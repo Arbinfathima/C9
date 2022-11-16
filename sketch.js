cf8b867fcf32bcf322a23658d646be6082f602b7
Arbinfathima <85548256+Arbinfathima@users.noreply.github.com>
2022-11-16 19:32:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+var box;
+
+function setup() {
+  createCanvas(800, 800);
+
+  box=createSprite(400,400,200,200);
+
+  console.log("hi")
+}
+
+
+
+
+function draw() {
+  background("pink");
+
+
+  drawSprites();
+}
